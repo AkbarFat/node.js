f79ad9aa65cb071a431278b84acfe37734bbd19a
Akbar <akbarfathullaev@gmail.com>
2025-07-02 14:00:28 +0500

    Controllers: we will write a query inside getAllTasks function in controllers to fetch data from db.

---
+++ task-manager/starter/controllers/tasks.js
@@ -0,0 +1,25 @@
+const getAllTasks = (req, res) => {
+    res.send('all items')
+}
+const getTask = (req, res) => {
+    res.send('get single item')
+}
+const createTask = (req, res) => {
+    res.send('create task')
+}
+const updateTask = (req, res) => {
+    res.send('update task')
+}
+const deleteTask = (req, res) => {
+    res.send('delete task')
+}
+
+
+
+module.exports = {
+    getAllTasks, 
+    getTask, 
+    createTask, 
+    updateTask, 
+    deleteTask
+}
