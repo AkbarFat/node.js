586d1391b76ab83cd116c8d4d06082cfeeb8fd3b
Akbar <akbarfathullaev@gmail.com>
2025-07-29 16:11:18 +0500

    Start building a server

---
+++ 2-express-tutorial/app.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-type': 'text/plain' })
+    res.write('<h1>home page<h1>')
+    res.end()
+})
+
+server.listen(3000)
