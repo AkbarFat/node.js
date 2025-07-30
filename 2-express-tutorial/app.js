7d6d143f6c24985d388da1b2139aa43c2d6ea110
Akbar <akbarfathullaev@gmail.com>
2025-07-30 13:56:56 +0500

    Simple page servings

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,9 +1,23 @@
 const http = require('http')
+const {readFileSync} = require('fs')
+
+const homePage = readFileSync('./someFrontendApp/index.html')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-type': 'text/plain' })
-    res.write('<h1>home page<h1>')
-    res.end()
+    const url = req.url
+    if (url === '/') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write(homePage)
+        res.end()
+    }else if (url === '/about') {
+        res.writeHead(200, { 'content-type': 'text/html' })
+        res.write('<h1>about page<h1>')
+        res.end()
+    }else{
+        res.writeHead(404, { 'content-type': 'text/html' })
+        res.write('<h1>page not found</h1>')
+        res.end()
+    }
 })
 
 server.listen(3000)
